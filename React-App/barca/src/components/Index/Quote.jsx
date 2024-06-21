6398eb3782857d4abe71286a26af21c29378a414
Karlo Buhinjak <buhinjak.k@gmail.com>
2024-06-21 13:44:40 +0200

    updated index page

--- React-App/barca/src/components/Index/Quote.jsx
+++ React-App/barca/src/components/Index/Quote.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
-const Quote = () => {
+const Quote = ({ quote }) => {
   return (
     <div class="quote-content">
       <figure class="quote">
-        <blockquote>
-          "Legend has it that when Franco's troops crushed Catalonia in 1939,
-          relegating it for the next thirty-five years to abuse and neglect, one
-          of his generals was asked what more he could possibly want now that he
-          had Barcelona.'"
-        </blockquote>
+        <blockquote>{quote}</blockquote>
         <figcaption>&mdash; Michael Paterniti</figcaption>
       </figure>
     </div>
--- React-App/barca/src/pages/Index.jsx
+++ React-App/barca/src/pages/Index.jsx
@@ -5,11 +5,15 @@ import Gallery from "../components/Index/Gallery";
 import Quote from "../components/Index/Quote";
 
 function Index() {
+  let quote = `Legend has it that when Franco's troops crushed Catalonia in 1939,
+  relegating it for the next thirty-five years to abuse and neglect, one
+  of his generals was asked what more he could possibly want now that he
+  had Barcelona.`;
   return (
     <div>
       <Title />
       <Gallery />
-      <Quote />
+      <Quote quote={quote} />
     </div>
   );
 }
